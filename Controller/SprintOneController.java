9b7ffa4e8c75ef6e6241c014f7ea526971ed290c
Micah Woods <micahwoods97@gmail.com>
2018-04-10 10:12:42 -0400

    Update SprintOneController.java

--- Controller/SprintOneController.java
+++ Controller/SprintOneController.java
@@ -3,6 +3,7 @@ package edu.ung.cis.cis3000.SprintOne.controller;
 public class SprintOneController {
 	//variables
 	@javafx.fxml.FXML private Label DateTime; 
+	@FXML private TextField CurrentGlucoseLevel; 
 	int amountTaken, currentLevel;
 
 	void basalAction(int amount)
