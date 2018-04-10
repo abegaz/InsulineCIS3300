513a176f12d644ddea475f4082f571d8f4e5ee63
Jack <35941702+jrileyhayes@users.noreply.github.com>
2018-04-10 09:55:04 -0400

    Add files via upload

---
+++ Controller/SprintOneController.java
@@ -0,0 +1,13 @@
+package edu.ung.cis.cis3000.SprintOne.controller;
+
+public class SprintOneController {
+	//variables
+	int amountTaken, currentLevel;
+
+	void basalAction(int amount)
+	{
+		amount = amountTaken;
+
+	}
+
+}
