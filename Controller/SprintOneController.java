99a36cca29f8bcc2a927b2397733eaed8ecab5f2
Jack <35941702+jrileyhayes@users.noreply.github.com>
2018-04-10 10:16:30 -0400

    Update SprintOneController.java

--- Controller/SprintOneController.java
+++ Controller/SprintOneController.java
@@ -1,15 +1,40 @@
 package edu.ung.cis.cis3000.SprintOne.controller;
 
-public class SprintOneController {
-	//variables
-	@javafx.fxml.FXML private Label DateTime; 
-	@FXML private TextField CurrentGlucoseLevel; 
-	int amountTaken, currentLevel;
+import javafx.scene.control.TextField;
 
-	void basalAction(int amount)
+public class SprintOneController {
+
+	//fx:id tags
+	@FXML private TextField amountTaken;
+	@FXML private TextField basalbutton;
+	@FXML private TextField whenTaken;
+	@FXML private TextField actionDisplay;
+	@FXML private TextField preActionLevel;
+	@FXML private TextField postActionLevel;
+	@FXML private TextField amountDisplay;
+
+
+
+	public void basalAction(int amount)//administer basal insulin
 	{
-		amount = amountTaken;
 
 	}
+
+	public void ontouch()//stop button
+	{
+
+	}
+
+	public void eatFood()//eat food button
+	{
+
+	}
+
+	public void setAmountTaken(int amount)//bolus insulin button
+	{
+		amount = amountTaken;
+		currentLevel = currentLevel + amount;
+	}
+
 
 }
