7928cf990cec22a2772c554d5e681a317e4d2f65
Jack <35941702+jrileyhayes@users.noreply.github.com>
2018-04-23 13:21:15 -0400

    Update Sprint02Model.java

--- Model/Sprint02Model.java
+++ Model/Sprint02Model.java
@@ -1,6 +1,6 @@
 package edu.ung.cis.cis3000.Sprint01.model;
 
-import javafx.application.Application;
+/*import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -9,61 +9,51 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
+import javafx.stage.Stage;*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.beans.property.SimpleStringProperty;
 
 public class Sprint02Model {
-
-	   private final TableView table = new TableView();
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
-
-	    @Override
-	    public void start(Stage stage) {
-	        Scene scene = new Scene(new Group());
-	        stage.setTitle("Insulin Pump");
-	        stage.setWidth(300);
-	        stage.setHeight(500);
-
-	        final Label label = new Label("Address Book");
-	        label.setFont(new Font("Arial", 20));
-
-	        table.setEditable(true);
-
-	        TableColumn firstNameCol = new TableColumn("First Name");
-	        TableColumn lastNameCol = new TableColumn("Last Name");
-	        TableColumn emailCol = new TableColumn("Email");
-
-	        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
-
-	        final VBox vbox = new VBox();
-	        vbox.setSpacing(5);
-	        vbox.setPadding(new Insets(10, 0, 0, 10));
-	        vbox.getChildren().addAll(label, table);
-
-	        ((Group) scene.getRoot()).getChildren().addAll(vbox);
-
-	        stage.setScene(scene);
-	        stage.show();
-	    }
-
-
-	//---------------------------------------------------------------------------------------------------
+	String url = "jdbc:mysql://localhost:3306/cisinsulinpump";
+	String user = "root";
+	String password = "";
+	Connection myConn = null;
+	Statement myStmt = null;
+	ResultSet myRs = null;
+	private int insulinCounter = 0;
+	private int offset = 0;
+	private int lastBS;
+	private int secondlastBS;
+	private int thirdlastBS;
+	private int fourthlastBS;
+    private int bsValue; // bs= BloodSugar
 	private String basalAmount;
 	private String foodAmount;
 	private String bolusAmount;
 	private String PLACEHOLDER;
 	private int patientId;
 
+	private final SimpleStringProperty firstName = new SimpleStringProperty("");
+	private final SimpleStringProperty lastName = new SimpleStringProperty("");
+	private final SimpleStringProperty email = new SimpleStringProperty("");
 
+/*
 	public int getPatientId() {
 		return patientId;
 	}
 	public void setPatientId(int patientId) {
 		this.patientId = patientId;
 	}
+*/
 
 
+/*
 	public Sprint02Model(int patientId) {
 		super();
 		this.patientId = patientId;
@@ -75,7 +65,11 @@ public class Sprint02Model {
 		this.bolusAmount = bolusAmount;
 		this.PLACEHOLDER = PLACEHOLDER;
 	}
-
+*/
+	//Constructor
+	public Sprint02Model(){
+		this.bsValue = this.generateBS();
+	}
 
 	public String getBasalAmount() {
 		return basalAmount;
@@ -85,6 +79,10 @@ public class Sprint02Model {
 	}
 
 
+	public int eatFood(){
+		this.bsValue += 50;
+		return this.bsValue;
+	}
 	public String getFoodAmount() {
 		return foodAmount;
 	}
@@ -93,6 +91,10 @@ public class Sprint02Model {
 	}
 
 
+	public int injectBolus(){
+		this.bsValue = this.generateBS(bsValue, true);
+		return this.bsValue;
+	}
 	public String getBolusAmount() {
 		return bolusAmount;
 	}
@@ -107,7 +109,50 @@ public class Sprint02Model {
 	public void setPLACEHOLDER(String party) {
 		this.PLACEHOLDER = party;
 	}
+//------------------------------------------------------------------------------------------------------------
+	//generic method to get insulin levels and create new data point
+	public int getBS(boolean createsNew){
+		if(createsNew)
+			this.bsValue = this.generateBS(this.bsValue, false);
+		return this.bsValue;
+	}
+	//this method will only be invoked for very first BS measurement
+	private int generateBS(){
+		return (int)(Math.random()*151) + 50;
+	}
+	//overloading previous method. Used for any subsequent BS measurement
+	private int generateBS(int prevBS, boolean withInjection){
+		int bs;
+		if(withInjection){
+			this.insulinCounter = 4;
+			this.offset = -24;
+		}
 
+		//variable just serves to create a probability distribution of how much next BS value will deviate
+		int dist = (int)(Math.random()*99);
+		//90% probability of next BS being normal fluctuation:
+		//{previous BS+(x | x<=25), previous BS-(x | x<=20)}
+		if(dist<35)
+			bs = (int)(Math.random()*25)+prevBS+offset; //increasing => person ate something
+		else if(dist<90)
+			bs = prevBS-(int)(Math.random()*20)+offset; //decrease due to normal bodily function
+
+		//10% probability of next BS being a spike:
+		//{previous BS+(x | x<=35), previous BS-(x | x<=25)}
+		else if(dist<97)
+			bs = (int)(int)(Math.random()*35)+prevBS+offset; //person ate a lot/ food with high glycemic index
+		else
+			bs = prevBS-(int)(Math.random()*25)+offset; //person may have exercised
+
+		if(insulinCounter != 0){
+			insulinCounter--;
+			if(insulinCounter==0)
+				this.offset=0;
+			//offset decreases as blood insulin levels decrease
+			this.offset/=2;
+		}
+		return bs;
+	}
 /*
 	public String getWhenTaken() {
 		return whenTaken;
@@ -119,5 +164,175 @@ public class Sprint02Model {
 		this.whentaken = whenTaken;
 	}
 */
+	 public void uploadBSToDB()
+	 {
+		 try {
+				myConn = DriverManager.getConnection(url, user, password);
+				myStmt = myConn.createStatement();
+				String sql = "insert into info " + " (bloodSugar)"
+						+ " values ('"+bsValue+"')";
+				myStmt.executeUpdate(sql);
+				System.out.println("Insert complete.");
+				} catch (Exception exc) {
+					exc.printStackTrace();
+				} finally {
+					if (myStmt != null) {
+						try {
+							myStmt.close();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					if (myConn != null) {
+						try {
+							myConn.close();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+		}
+	 }
+
+	 public int getlastBSfromDB() throws SQLException{
+		  try {
+		   myConn = DriverManager.getConnection(url, user , password);
+		   myStmt = myConn.createStatement();
+		   myRs = myStmt.executeQuery("SELECT * FROM info WHERE id IN (SELECT MAX(id) FROM info)");
+		   while (myRs.next()) {
+		   //System.out.println(myRs.getString("bloodSugar") + "  " + myRs.getString("time"));
+		   lastBS = Integer.parseInt(myRs.getString("bloodSugar"));
+		   }
+		  }
+		  catch (Exception exc) {
+		   exc.printStackTrace();
+		  }
+		  finally {
+		   if (myRs != null) {
+		    myRs.close();
+		   }
+
+		   if (myStmt != null) {
+		    myStmt.close();
+		   }
+
+		   if (myConn != null) {
+		    myConn.close();
+		   }
+
+		  }
+		  return lastBS;
+		 }
+
+
+	 public int getlastBS() {
+		return lastBS;
+	 }
+
+
+	public int get2ndlastBSfromDB() throws SQLException{
+		  try {
+		   myConn = DriverManager.getConnection(url, user , password);
+		   myStmt = myConn.createStatement();
+		   myRs = myStmt.executeQuery("SELECT * FROM info ORDER BY id DESC LIMIT 1,1");
+		   while (myRs.next()) {
+		   // System.out.println(myRs.getString("bloodSugar") + "  " + myRs.getString("time"));
+		    secondlastBS = Integer.parseInt(myRs.getString("bloodSugar"));
+		   }
+		  }
+		  catch (Exception exc) {
+		   exc.printStackTrace();
+		  }
+		  finally {
+		   if (myRs != null) {
+		    myRs.close();
+		   }
+
+		   if (myStmt != null) {
+		    myStmt.close();
+		   }
+
+		   if (myConn != null) {
+		    myConn.close();
+		   }
+		  }
+		  return secondlastBS;
+		 }
+
+
+	public int get2ndlastBS() {
+		return secondlastBS;
+	 }
+
+
+	public int get3rdlastBSfromDB() throws SQLException{
+		  try {
+		   myConn = DriverManager.getConnection(url, user , password);
+		   myStmt = myConn.createStatement();
+		   myRs = myStmt.executeQuery("SELECT * FROM info ORDER BY id DESC LIMIT 1,2");
+		   while (myRs.next()) {
+		   // System.out.println(myRs.getString("bloodSugar") + "  " + myRs.getString("time"));
+		    thirdlastBS = Integer.parseInt(myRs.getString("bloodSugar"));
+		   }
+		  }
+		  catch (Exception exc) {
+		   exc.printStackTrace();
+		  }
+		  finally {
+		   if (myRs != null) {
+		    myRs.close();
+		   }
+
+		   if (myStmt != null) {
+		    myStmt.close();
+		   }
+
+		   if (myConn != null) {
+		    myConn.close();
+		   }
+		  }
+			return thirdlastBS;
+		 }
+
+
+	public int get3rdlastBS() {
+		return thirdlastBS;
+	}
+
+
+	public int get4thlastBSfromDB() throws SQLException{
+		  try {
+		   myConn = DriverManager.getConnection(url, user , password);
+		   myStmt = myConn.createStatement();
+		   myRs = myStmt.executeQuery("SELECT * FROM info ORDER BY id DESC LIMIT 1,3");
+		   while (myRs.next()) {
+		   // System.out.println(myRs.getString("bloodSugar") + "  " + myRs.getString("time"));
+		    fourthlastBS = Integer.parseInt(myRs.getString("bloodSugar"));
+		   }
+		  }
+		  catch (Exception exc) {
+		   exc.printStackTrace();
+		  }
+		  finally {
+		   if (myRs != null) {
+		    myRs.close();
+		   }
+
+		   if (myStmt != null) {
+		    myStmt.close();
+		   }
+
+		   if (myConn != null) {
+		    myConn.close();
+		   }
+		  }
+		  return fourthlastBS;
+		 }
+
+
+	public int get4thlastBS() {
+		return fourthlastBS;
+		}
 
 }
