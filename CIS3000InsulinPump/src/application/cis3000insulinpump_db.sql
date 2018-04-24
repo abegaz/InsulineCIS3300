77c77d4b25eef2047ffb5ea5f248c791de1c260e
Jack <35941702+jrileyhayes@users.noreply.github.com>
2018-04-24 10:29:51 -0400

    Update cis3000insulinpump_db.sql

--- CIS3000InsulinPump/src/application/cis3000insulinpump_db.sql
+++ CIS3000InsulinPump/src/application/cis3000insulinpump_db.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
--- https://www.phpmyadmin.net/
+-- version 4.4.14
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2018 at 09:21 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.4
+-- Generation Time: Apr 24, 2018 at 04:27 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,11 +26,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `bloodsugar`
 --
 
-CREATE TABLE `bloodsugar` (
-  `bolusAmount` int(10) NOT NULL,
-  `foodAmount` int(10) NOT NULL,
-  `basalAmount` int(10) NOT NULL,
-  `patientId` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `bloodsugar` (
+  `bolusAmount` varchar(10) NOT NULL,
+  `foodAmount` varchar(10) NOT NULL,
+  `basalAmount` varchar(10) NOT NULL,
+  `patientId` varchar(11) NOT NULL,
   `dateTime` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -45,17 +43,6 @@ CREATE TABLE `bloodsugar` (
 --
 ALTER TABLE `bloodsugar`
   ADD PRIMARY KEY (`patientId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `bloodsugar`
---
-ALTER TABLE `bloodsugar`
-  MODIFY `patientId` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
