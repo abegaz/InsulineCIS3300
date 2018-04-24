52dd1f3430fd23886ea3f96cb83a72e878175fde
Jack <35941702+jrileyhayes@users.noreply.github.com>
2018-04-24 01:22:52 -0400

    Create DatabaseConfig.java

---
+++ CIS3000InsulinPump/src/application/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package application;
+
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+
+	public class DatabaseConfig {
+
+		private static final String USERNAME = "root";
+		private static final String PASSWORD = "";
+		private static final String CONN_STRING = "jdbc:mysql://localhost/easypoll";
+
+		// connection  method that connects us to the MySQL database
+		public static Connection getConnection() throws SQLException{
+			//System.out.println("Connected to student database successfully!");
+			return DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+		}
+
+		// method that displays our errors in more detail if connection fails
+		public static void displayException(SQLException ex){
+
+			System.err.println("Error Message: " + ex.getMessage());
+			System.err.println("Error Code: " + ex.getErrorCode());
+			System.err.println("SQL Status: " + ex.getSQLState());
+
+		}
+	}
+
