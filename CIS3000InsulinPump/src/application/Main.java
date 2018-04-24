659415853349282d30eceb456298390b96053a8d
Jack <35941702+jrileyhayes@users.noreply.github.com>
2018-04-24 01:19:13 -0400

    Create Main.java
    this is derived from the EasyPoll Example

---
+++ CIS3000InsulinPump/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage){
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("CIS3000InsulinPumpView.fxml"));
+			Scene scene = new Scene(root,800,600);
+			primaryStage.setTitle("CIS 3300 Starter Project");
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
